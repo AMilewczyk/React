f1a8569d0a112cb3d2f8e58b421f3f133a7d04c8
Andrzej <andrzej.milewczyk1@gmail.com>
2021-03-29 15:00:17 +0200

    components

---
+++ src/components/MyInfo.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+function MyInfo() {
+    return(
+        <div>
+            <h1> Andrzej</h1>
+            <p> I am the godest</p>
+
+        <ul>
+            <ui>1</ui>
+            <ui>2</ui>
+            <ui>3</ui>
+        </ul>
+        </div>
+    )
+}
+
+export default MyInfo
+
+
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+function Navbar() {
+    return(
+        <navbar> it is navbar </navbar>
+    )
+}
+
+
+export default Navbar
